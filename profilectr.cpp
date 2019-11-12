c21e6be06535e711e892c07d2696e991e2d89dfc
kedar <kedarmail@gmail.com>
2019-11-11 19:33:02 -0800

    allow empty locations

--- profilectr.cpp
+++ profilectr.cpp
@@ -9,11 +9,11 @@ void profilectr::userinsert(name user, std::string display_name, std::string abo
 
     // Argument Checks
     eosio::check( display_name.size() <= MAX_DISPLAY_NAME_SIZE, "display name is too long. MAX_DISPLAY_NAME_SIZE= 30");
-    eosio::check( display_name.size() >= MIN_DISPLAY_NAME_SIZE, "display name must be at least 2 characters");
+    eosio::check( display_name.size() == 0 || display_name.size() >= MIN_DISPLAY_NAME_SIZE, "display name is too long. MIN_DISPLAY_NAME_SIZE= 3");
     eosio::check( about_me.size() <= MAX_ABOUTME_SIZE, "'about me' is too long. MAX_IPFS_SIZE=46");
+    eosio::check( about_me.size() == 0 || about_me.size() >= MIN_ABOUTME_SIZE, "display name is too long. MIN_DISPLAY_NAME_SIZE= 3");
     eosio::check( img.size() <= MAX_IMG_SIZE, "Image is too large. MAX_IMG_SIZE = 400 characters");
     eosio::check( location.country.size() <= MAX_COUNTRY_SIZE, "country code max size is 4 characters");
-    eosio::check( location.country.size() >= MIN_COUNTRY_SIZE, "country code must be at least 1 character");
     // Check language codes
     for(int i=0; i<languages.size(); ++i) {
         eosio::check( languages[i].lang_code.size() <= MAX_LANG_CODE_SIZE, "lang code max size is 8 bytes");
--- profilectr.hpp
+++ profilectr.hpp
@@ -8,6 +8,7 @@ using namespace eosio;
 // Constants
 const uint64_t MAX_IMG_SIZE = 400;
 const uint64_t MAX_ABOUTME_SIZE = 300;
+const uint64_t MIN_ABOUTME_SIZE = 3;
 const uint64_t MAX_LANG_CODE_SIZE = 7;
 const uint64_t MIN_LANG_CODE_SIZE = 2;
 const uint64_t MAX_PLATFORM_SIZE = 20;
@@ -15,7 +16,7 @@ const uint64_t MIN_PLATFORM_SIZE = 4;
 const uint64_t MAX_COUNTRY_SIZE = 4;
 const uint64_t MIN_COUNTRY_SIZE = 1;
 const uint64_t MAX_DISPLAY_NAME_SIZE = 30;
-const uint64_t MIN_DISPLAY_NAME_SIZE = 2;
+const uint64_t MIN_DISPLAY_NAME_SIZE = 3;
 
 typedef struct address{
     std::string city;
